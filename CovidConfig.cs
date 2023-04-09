bacc738acef96092952f1070e373c5f545d893ff
phusallver <103794258+phusallver@users.noreply.github.com>
2023-04-09 10:17:01 +0700

    Menambahkan Class "CovidKonfig"

---
+++ CovidConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace mod8
+{
+    internal class CovidConfig
+    {
+        public Konfig konfig;
+        private const string filepath = @"covid_config.json";
+
+        public CovidConfig()
+        {
+            try
+            {
+                ReadKonfigFile();
+            }
+            catch
+            {
+                SetDefault();
+                WriteKonfigFile();
+            }
+        }
+        public void ReadKonfigFile()
+        {
+            string hasilBaca = File.ReadAllText(filepath);
+            konfig = JsonSerializer.Deserialize<Konfig>(hasilBaca);
+        }
+        public void WriteKonfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            string tulisan = JsonSerializer.Serialize(konfig, options);
+            File.WriteAllText(filepath, tulisan);
+        }
+        public void SetDefault()
+        {
+            konfig = new Konfig();
+            konfig.satuan_suhu = "celcius";
+            konfig.batas_hari_demam = 14;
+            konfig.pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+            konfig.pesan_diterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+        }
+        public void UbahSatuan()
+        {
+            konfig.satuan_suhu = (konfig.satuan_suhu == "celcius") ?
+                "fahrenheit" : "celcius";
+        }
+    }
+}
+public class Konfig
+{
+    public string satuan_suhu { get; set; }
+    public int batas_hari_demam { get; set; }
+    public string pesan_ditolak { get; set; }
+    public string pesan_diterima { get; set; }
+    public Konfig() { }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,58 @@
-﻿internal class Program
+﻿using mod8;
+
+public class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CovidConfig covig = new CovidConfig();
+
+        Console.Write("Berapa suhu badan anda saat ini?" +
+            " Dalam nilai "+covig.konfig.satuan_suhu+" ");
+        double suhu_sekarang = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Berapa hari yang lalu (perkiraan) " +
+            "anda terakhir memiliki gejala demam? ");
+        int gejala_demam = Convert.ToInt32(Console.ReadLine());
+        string pesan_keluar = covig.konfig.pesan_ditolak;
+        if (gejala_demam > covig.konfig.batas_hari_demam)
+        {
+            if (covig.konfig.satuan_suhu == "celcius")
+            {
+                pesan_keluar = (suhu_sekarang >= 36.5 && suhu_sekarang <= 37.5) ?
+                    covig.konfig.pesan_diterima : covig.konfig.pesan_ditolak;
+            }else if(covig.konfig.satuan_suhu == "fahrenheit")
+            {
+                pesan_keluar = (suhu_sekarang >= 97.7 && suhu_sekarang <= 99.5) ?
+                    covig.konfig.pesan_diterima : covig.konfig.pesan_ditolak;
+            }
+        }        
+        Console.WriteLine(pesan_keluar);
+
+        Console.WriteLine("\n\n");
+        Console.WriteLine("---========Mengubah satuan ke fahhrenheit======---");
+        Console.WriteLine("\n\n");
+
+        covig.UbahSatuan();
+        Console.Write("Berapa suhu badan anda saat ini?" +
+            " Dalam nilai " + covig.konfig.satuan_suhu + " ");
+        suhu_sekarang = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Berapa hari yang lalu (perkiraan) " +
+            "anda terakhir memiliki gejala demam? ");
+        gejala_demam = Convert.ToInt32(Console.ReadLine());
+        pesan_keluar = covig.konfig.pesan_ditolak;
+        if (gejala_demam > covig.konfig.batas_hari_demam)
+        {
+            if (covig.konfig.satuan_suhu == "celcius")
+            {
+                pesan_keluar = (suhu_sekarang >= 36.5 && suhu_sekarang <= 37.5) ?
+                    covig.konfig.pesan_diterima : covig.konfig.pesan_ditolak;
+            }
+            else if (covig.konfig.satuan_suhu == "fahrenheit")
+            {
+                pesan_keluar = (suhu_sekarang >= 97.7 && suhu_sekarang <= 99.5) ?
+                    covig.konfig.pesan_diterima : covig.konfig.pesan_ditolak;
+            }
+        }
+        Console.WriteLine(pesan_keluar);
     }
 }
+
